7b434f44eec8601e3bb66f008895656e2a444a64
ankit saini <as600030@gmail.com>
2017-08-03 13:06:16 +0530

    add files

--- micro/js/controllers/MainController.js
+++ micro/js/controllers/MainController.js
@@ -1,6 +1,11 @@
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
   forecast.success(function(data) {
     $scope.fiveDay = data;
-  });
+      $scope.show = 'msg1';
+	$scope.toggle = function() {
+	    $scope.show = $scope.show == 'msg1' ? 'msg2' : 'msg1';
+	
+	};     
+});
 }]);
 
